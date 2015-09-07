fade32bd6e2fd4e4191346c540ae39e7ef86f8cf
gardners <paul@servalproject.org>
2015-09-07 20:40:07 +0930

    clean up in preparation for providing only a http client libary for inclusion in LBARD.

--- src/dbg.h
+++ src/dbg.h
@@ -12,4 +12,11 @@
 #define jump_unless(A) if (!(A)) { goto error; }
 #define error_unless(A, M, ...) if (!(A)) { fprintf(stderr, M "\n", ##__VA_ARGS__); goto error; }
 
+#define clean_errno() (errno == 0 ? "None" :  strerror(errno))
+
+#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s (in function '%s'):%d: errno: %s) " M "\n", __FILE__, __FUNCTION__, __LINE__, clean_errno(), ##__VA_ARGS__)
+
+#define jump_to_error_if(A) if (A) { goto error; }
+#define jump_to_error_unless(A) if (!(A)) { goto error; }
+
 #endif
